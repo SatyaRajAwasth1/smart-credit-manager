1af5efe6be08300bc526d9268d5b544295e63476
SatyaRajAwasth1 <awasthisatyaraj2060@gmail.com>
2023-11-12 10:17:51 +0545

    feat: #7 implemented credential dashboard

---
+++ src/main/java/np/com/satyarajawasthi/smartcreditmanager/DashboardApp.java
@@ -0,0 +1,25 @@
+package np.com.satyarajawasthi.smartcreditmanager;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class DashboardApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/np/com/satyarajawasthi/smartcreditmanager/fxml/dashboard.fxml")));
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.setTitle("Smart Credit Manager");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
